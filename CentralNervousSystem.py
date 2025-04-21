198f1ab72efc2fee0fb713053b2655cde853e8b2
Taireru LLC ™ <tairerullc@gmail.com>
2025-04-21 11:22:10 -0500

    Add files via upload

---
+++ CentralNervousSystem.py
@@ -0,0 +1,26 @@
+"""Main file for Tai AI, a self-evolving AI."""
+# Imports
+from brain.NeuralBlueprint import MODEL
+import brain.PrefrontalCortex as PrefrontalCortex
+import brain.Thalamus as Thalamus
+import brain.Neuroplasticity as Neuroplasticity
+
+USE_EXIT: bool = True
+
+# Initialize the API
+Thalamus.init()
+
+# Load the model and configure PyTai
+model = PrefrontalCortex.init(MODEL)
+py_tai = False
+
+# Main loop with exception handling
+if __name__ == "__main__":
+    try:
+        if not py_tai:
+            PrefrontalCortex.start_ui(model)
+        else:
+            Neuroplasticity.py_tai()
+    except Exception as e:
+        # Handle any unexpected exceptions
+        print(f"Error: {e}")
