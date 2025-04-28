75360582290e099da755e6c6139536ebe5a4abec
Taireru LLC ™ <tairerullc@gmail.com>
2025-04-28 10:09:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 """Main file for Tai AI, a self-evolving AI."""
 # Imports
-from brain.NeuralBlueprint import MODEL
-import brain.PrefrontalCortex as PrefrontalCortex
-import brain.Thalamus as Thalamus
-import brain.Hippocampus as Hippocampus
+from brain.config import MODEL
+import brain.Dictator as Dictator
+import brain.api as api
+import brain.Architect as Architect
 
 USE_EXIT: bool = True
 
 # Initialize the API
-Thalamus.init()
+api.init()
 
 # Load the model and configure PyTai
-model = PrefrontalCortex.init(MODEL)
+model = Dictator.init(MODEL)
 py_tai = False
 
 # Main loop with exception handling
 if __name__ == "__main__":
     try:
         if not py_tai:
-            PrefrontalCortex.start_ui(model)
+            Dictator.start_ui(model)
         else:
-            Hippocampus.py_tai()
+            Architect.py_tai()
     except Exception as e:
         # Handle any unexpected exceptions
         print(f"Error: {e}")
